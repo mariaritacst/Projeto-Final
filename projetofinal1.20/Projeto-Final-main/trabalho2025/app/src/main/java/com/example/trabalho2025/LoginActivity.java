4a4d236683eb6ab72fbdfa00c3495f103d506bd3
mariaritacst <166262923+mariaritacst@users.noreply.github.com>
2025-04-16 07:28:43 -0300

    Update LoginActivity.java

--- projetofinal1.20/Projeto-Final-main/trabalho2025/app/src/main/java/com/example/trabalho2025/LoginActivity.java
+++ projetofinal1.20/Projeto-Final-main/trabalho2025/app/src/main/java/com/example/trabalho2025/LoginActivity.java
@@ -9,25 +9,26 @@ import com.example.trabalho2025.databinding.ActivityLoginBinding;
 // Classe responsável pela tela de login
 public class LoginActivity extends AppCompatActivity {
 
-	// Variável para o binding, que facilita a manipulação dos elementos da interface
-	private ActivityLoginBinding binding;
+    // Variável para o binding, que facilita a manipulação dos elementos da interface
+    private ActivityLoginBinding binding;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    	// Infla o layout usando ViewBinding
-    	binding = ActivityLoginBinding.inflate(getLayoutInflater());
-    	setContentView(binding.getRoot()); // Define o layout da activity
+        // Infla o layout usando ViewBinding
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // Define o layout da activity usando o root do binding
 
-    	// Configuração do clique no botão de login
-    	binding.btnLogin.setOnClickListener(new View.OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-            	// Cria uma intenção para navegar para a MainActivity
-            	Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            	startActivity(intent); // Inicia a MainActivity
-            	finish(); // Fecha a LoginActivity para que o usuário não volte ao login ao pressionar "Voltar"
-        	}
-    	});
-	}
+        // Configuração do clique no botão de login
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cria uma intenção para navegar para a MainActivity
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent); // Inicia a MainActivity
+                finish(); // Fecha a LoginActivity para que o usuário não volte ao login ao pressionar "Voltar"
+            }
+        });
+    }
+}
